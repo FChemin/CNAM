56ecf11c564f2b2f9a7e430f9f9d98a3d0716319
Florian Chemin <chemin.florian@gmail.com>
2018-11-05 10:53:47 +0100

    Update tp2_1.c

--- NFA037/Jalon2/tp2_1.c
+++ NFA037/Jalon2/tp2_1.c
@@ -1,3 +1,16 @@
+/*exo_printf.c
+
+Exercice 2_1
+
+Un programme utilisant la sortie standard pour afficher la taille d'un entier
+et le premier et dernier caractère d'une chaîne
+
+Auteur : Florian Chemin
+Date : 16 octobre 2018
+Cours : NFA037
+Établissement : CNAM Bretagne
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
