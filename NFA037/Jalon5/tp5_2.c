5deca7f974c7e293d1f7a793577099c9ca585c42
Florian Chemin <19800396+FChemin@users.noreply.github.com>
2018-11-08 14:06:58 +0100

    Create tp5_2.c

---
+++ NFA037/Jalon5/tp5_2.c
@@ -0,0 +1,50 @@
+/*symetrie.c
+
+Exercice 5_2
+
+Un programme comprenant une fonction prennant en argument un point
+et renvoyant un point du même nom et de coordonnées opposées.
+
+Auteur : Florian Chemin
+Date : 8 novembre 2018
+Cours : NFA037
+Établissement : CNAM Bretagne
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct point
+{
+    char nom;
+    int x, y;
+};
+
+struct point calculerOppose(struct point p);
+
+int main(int argc, char *argv[])
+{
+    struct point pointA, pointB;
+
+    pointA.nom = 'A';
+    pointA.x = 10;
+    pointA.y = 12;
+
+    pointB = calculerOppose(pointA);
+
+    printf("Soit le point %c (%d;%d).\n", pointA.nom, pointA.x, pointA.y);
+    printf("Son opposé est le point %c (%d;%d)\n\n", pointA.nom, pointB.x, pointB.y);
+
+    return 0;
+}
+
+struct point calculerOppose(struct point p)
+{
+    struct point q;
+    int x, y;
+
+    q.x = 0 - p.x;
+    q.y = 0 - p.y;
+
+    return q;
+}
