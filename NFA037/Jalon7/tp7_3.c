595d65e84efdb26697a1e6c7ced03cc33296bae8
Florian Chemin <19800396+FChemin@users.noreply.github.com>
2018-12-13 10:56:36 +0100

    Update tp7_3.c

--- NFA037/Jalon7/tp7_3.c
+++ NFA037/Jalon7/tp7_3.c
@@ -44,16 +44,20 @@ char* code[] = {
     "BABBB", // Z
     };
 
-void coder(char* texteACamoufler, char* texteDeCamouflage);
-char* decoder(char* texteADecoder);
+void coder(char* texteACamoufler, char* texteDeCamouflage); // Prototype de la fonction coder
+char* decoder(char* texteADecoder); // Prototype de la fonction decoder
 
 int main(int argc, char *argv[])
 {
+    /*Déclaration et allocaiton mémoire des pointeurs*/
+
     char* texteACamoufler = NULL;
     char* texteDeCamouflage = NULL;
 
     texteACamoufler = (char*)malloc(6);
     texteDeCamouflage = (char*)malloc(26);
+
+    /*Vérification de l'allocation mémoire*/
 
     if (texteACamoufler == NULL)
     {
@@ -65,6 +69,8 @@ int main(int argc, char *argv[])
         exit(0);
     }
 
+    /*Récupération des entrées utilisateurs*/
+
     printf("Entrez le texte à camoufler : ");
     scanf("%5s", texteACamoufler);
     printf("Entrez le texte de camouflage : ");
@@ -74,13 +80,15 @@ int main(int argc, char *argv[])
     printf("Codage :\n");
     printf("--------\n\n");
 
-    coder(texteACamoufler, texteDeCamouflage);
+    coder(texteACamoufler, texteDeCamouflage); //Appel de la fonction coder
 
     printf("\n\n");
     printf("Décodage :\n");
     printf("--------\n\n");
 
-    decoder(texteDeCamouflage);
+    decoder(texteDeCamouflage); // Appel de la fonction decoder
+
+    /*Libération de la mémoire allouée aux pointeurs*/
 
     free(texteACamoufler);
     free(texteDeCamouflage);
@@ -88,7 +96,7 @@ int main(int argc, char *argv[])
     return 0;
 }
 
-void coder(char* texteACamoufler, char* texteDeCamouflage)
+void coder(char* texteACamoufler, char* texteDeCamouflage) //Définition de la fonction coder
 {
     char* texteIntermediaire = NULL;
 	int i;
@@ -100,12 +108,16 @@ void coder(char* texteACamoufler, char* texteDeCamouflage)
         exit(0);
     }
 
+    /*Codage du texte à camoufler*/
+
     for (i = 0; i < strlen(texteACamoufler); i++)
 	{
         strcat(texteIntermediaire, code[toupper(texteACamoufler[i]) - 'A']);
 	}
 
 	printf("Texte intermédiaire : %s\n", texteIntermediaire);
+
+	/*Transformation du texte de camouflage*/
 
 	for (i = 0; i < strlen(texteIntermediaire); i++)
 	{
@@ -123,7 +135,7 @@ void coder(char* texteACamoufler, char* texteDeCamouflage)
 	printf("Texte final : %s\n", texteDeCamouflage);
 }
 
-char* decoder(char* texteADecoder)
+char* decoder(char* texteADecoder) //Définition de la fonction decoder
 {
     char* texteIntermediaire = NULL;
     char* texteDecode = NULL;
@@ -142,6 +154,8 @@ char* decoder(char* texteADecoder)
         exit(0);
     }
 
+    /*Transformation du texte de camouflage à décoder en un texte codé*/
+
     for (i = 0; i < strlen(texteADecoder); i++)
 	{
         if(isupper(texteADecoder[i]))
@@ -153,6 +167,9 @@ char* decoder(char* texteADecoder)
             strcat(texteIntermediaire, "B");
         }
 	}
+
+	/*Comparaison par groupe de cinq lettres du texte codé avec l'alphabet codé
+	et décodage lorsqu'un groupe de cinq lettres correspond à une lettre codé*/
 
 	for (i = 0; i < strlen(texteADecoder)/5; i++)
 	{
